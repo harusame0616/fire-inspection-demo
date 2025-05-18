ec8bb8c3763c9d338f09d02f04482804f39c151e
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-05-19 00:59:34 +0900

    サンプル実装

---
+++ src/app/inspect.ts
@@ -0,0 +1,66 @@
+"use server";
+import { OpenAI } from "openai";
+import { z } from "zod";
+import { zodResponseFormat } from "openai/helpers/zod";
+import type { InspectionResult } from "./type";
+type InspectActionParams = {
+	office: string;
+	address: string;
+	zoning: string;
+	constructionType: string;
+	totalFloorArea: number;
+};
+
+const inspectionSchema = z.object({
+	automaticFireAlarmSystem: z.object({
+		installationObligation: z.boolean(),
+		basisOfJudgment: z.string(),
+		installationStandard: z.string(),
+		structureSpecification: z.string(),
+	}),
+});
+
+export async function inspectAction({
+	office,
+	address,
+	zoning,
+	constructionType,
+	totalFloorArea,
+}: InspectActionParams) {
+	const openai = new OpenAI();
+
+	const response = await openai.chat.completions.create({
+		model: "gpt-4o-mini",
+		response_format: zodResponseFormat(inspectionSchema, "inspection"),
+
+		messages: [
+			{
+				role: "system",
+				content: `あなたは日本の消防関連の法律に精通しています。ユーザーからの報告を元に以下を審査してください。
+                報告は JSON 形式で行ってください
+
+                ■ 消防法上必要な消防設備・管理制度（＋規格）
+                1. 自動火災報知設備
+                   1.1 設置義務のあり、無し
+                   1.2 判定の根拠法令（例：消防法施行令 第32条 第１項 第１号）
+                   1.3 設置基準（例：１項イ（病院等）で述べ面積301m²以上 → 義務あり
+                   1.4 構造・・性能規格：消防法施行規則 第24条〜第26条、消防用設備等の技術上の基準（告示第３４号）
+                `,
+			},
+			{
+				role: "user",
+				content: `
+                事務所名: ${office}
+                住所: ${address}
+                用途地域: ${zoning}
+                工事種別: ${constructionType}
+                延べ面積: ${totalFloorArea} ㎡
+                `,
+			},
+		],
+	});
+
+	console.dir(response, { depth: null });
+
+	return JSON.parse(response.choices[0].message.content) as InspectionResult;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,202 @@
-import Image from "next/image";
+"use client";
+
+import * as v from "valibot";
+import { useForm } from "react-hook-form";
+import { valibotResolver } from "@hookform/resolvers/valibot";
+import {
+	Form,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormControl,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { ConstructionType, type InspectionResult } from "./type";
+import { inspectAction } from "./inspect";
+import { useState } from "react";
+import { RotateCcw, RotateCw } from "lucide-react";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
+const inputSchema = v.object({
+	office: v.string(),
+	address: v.string(),
+	zoning: v.string(),
+	constructionType: v.enum(ConstructionType),
+	totalFloorArea: v.pipe(v.string(), v.transform(Number), v.number()),
+});
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+	const [inspectionResult, setInspectionResult] =
+		useState<InspectionResult | null>(null);
+	const form = useForm({
+		resolver: valibotResolver(inputSchema),
+	});
 
-    </div>
-  );
+	const onSubmit = async (data: v.InferOutput<typeof inputSchema>) => {
+		const result = await inspectAction(data);
+		setInspectionResult(result);
+	};
+
+	return (
+		<div className="p-4 grid grid-cols-5 gap-4 h-full">
+			<Card className="col-span-2">
+				<CardHeader>申請内容</CardHeader>
+				<CardContent>
+					<Form {...form}>
+						<form
+							className="flex flex-col gap-4"
+							onSubmit={form.handleSubmit(onSubmit)}
+						>
+							<FormField
+								control={form.control}
+								name="office"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>事務所名</FormLabel>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="address"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>住所</FormLabel>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="zoning"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>用途地域</FormLabel>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="constructionType"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>工事種別</FormLabel>
+										<FormControl>
+											<RadioGroup
+												onValueChange={field.onChange}
+												defaultValue={field.value}
+												className="flex flex-col gap-1"
+											>
+												<FormItem className="flex items-center space-x-3 space-y-0">
+													<FormControl>
+														<RadioGroupItem value={ConstructionType.New} />
+													</FormControl>
+													<FormLabel className="font-normal">新築</FormLabel>
+												</FormItem>
+												<FormItem className="flex items-center space-x-3 space-y-0">
+													<FormControl>
+														<RadioGroupItem
+															value={ConstructionType.Extension}
+														/>
+													</FormControl>
+													<FormLabel className="font-normal">増築</FormLabel>
+												</FormItem>
+												<FormItem className="flex items-center space-x-3 space-y-0">
+													<FormControl>
+														<RadioGroupItem
+															value={ConstructionType.Reconstruction}
+														/>
+													</FormControl>
+													<FormLabel className="font-normal">改築</FormLabel>
+												</FormItem>
+												<FormItem className="flex items-center space-x-3 space-y-0">
+													<FormControl>
+														<RadioGroupItem
+															value={ConstructionType.ChangeOfUse}
+														/>
+													</FormControl>
+													<FormLabel className="font-normal">
+														用途変更
+													</FormLabel>
+												</FormItem>
+											</RadioGroup>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="totalFloorArea"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>延べ面積（㎡）</FormLabel>
+										<FormControl>
+											<Input {...field} type="number" />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<Button type="submit">審査する</Button>
+						</form>
+					</Form>
+				</CardContent>
+			</Card>
+			<Card className="col-span-3">
+				<CardHeader>審査結果</CardHeader>
+				<CardContent>
+					{form.formState.isSubmitting ? (
+						<div className="flex items-center justify-center">
+							<RotateCw className="w-4 h-4 animate-spin" />
+						</div>
+					) : (
+						<div className="flex flex-col gap-2">
+							<div>
+								<h2>自動火災報知機</h2>
+								<ul>
+									<li>
+										設置義務：
+										{inspectionResult?.automaticFireAlarmSystem
+											.installationObligation
+											? "あり"
+											: "なし"}
+									</li>
+									<li>
+										判定の根拠法令：
+										{inspectionResult?.automaticFireAlarmSystem.basisOfJudgment}
+									</li>
+									<li>
+										設置基準：
+										{
+											inspectionResult?.automaticFireAlarmSystem
+												.installationStandard
+										}
+									</li>
+									<li>
+										構造・性能規格：
+										{
+											inspectionResult?.automaticFireAlarmSystem
+												.structureSpecification
+										}
+									</li>
+								</ul>
+							</div>
+						</div>
+					)}
+				</CardContent>
+			</Card>
+		</div>
+	);
 }
